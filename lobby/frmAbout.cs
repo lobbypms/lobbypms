02d808ba7b96c03ca7e041f6cc687fd0a34d7bd4
gabit0t0 <gabriel.lopardo@gmail.com>
2017-06-05 01:23:38 -0300

    frmAbout Remove comments FROM VS

--- lobby/frmAbout.cs
+++ lobby/frmAbout.cs
@@ -14,7 +14,6 @@ namespace lobby
         {
             var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
             label1.Text = "Lobby PMS v" + version.ToString();
-            //borrar
         }
 
         private void label2_Click(object sender, EventArgs e)
